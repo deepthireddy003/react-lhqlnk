cff6eba5451bf78d9ff9ced93f5ab43d2da5b210
deepthireddy003 <deepthireddyareksoft@gmail.com>
2021-07-28 12:39:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lhqlnk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lhqlnk)
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const [userInfo, stateUserInfo] = useState();
+  const onSubmit = data => {
+    stateUserInfo(data);
+    console.log(data);
+  };
+  return (
+    <div className="container">
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Registration Form</h1>
+        <div className="ui divider" />
+        <div className="ui form">
+          <div className="field">
+            <label>UserName</label>
+
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              {...register('username', { required: 'UserName is Required' })}
+            />
+          </div>
+          <p>{errors.username?.message}</p>
+        </div>
+        <div className="ui form">
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              {...register('email', { required: 'Email is Required' })}
+            />
+          </div>
+          <p>{errors.email?.message}</p>
+        </div>
+        <div className="ui form">
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              {...register('password', { required: 'Password is Required' })}
+            />
+          </div>
+          <p>{errors.password?.message}</p>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
